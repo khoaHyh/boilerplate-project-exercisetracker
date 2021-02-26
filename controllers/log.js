4edf319bc45960c5d5fa69d9c2b487fe08b9d66d
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-26 18:38:48 -0500

    Feat: Add query param conditionals
    Add conditionals to act when the query parameters: to, from, and limit
    are provided.

--- controllers/log.js
+++ controllers/log.js
@@ -1,18 +1,35 @@
 const User = require('../models/user');
 
+// test by using this format baseurl/api/exercise/log?userId=_idgoeshere&param1=p1&param2=p2
 const log = (req, res) => {
-    const userId = req.query.userId;
+    const { userId, from, to } = req.query;
+    let { limit } = req.query;
 
     User.findById(userId, (err, user) => {
         if (err) return console.error(`log error: ${err}`);
-        const { _id, username, log } = user;
+        const { _id, username } = user;
+        let { log } = user;         
         const count = log.length;
+
+        if (from) {
+            log = log.filter(item => { return item.date.getTime() >= Date.parse(from) });
+        }
+
+        if (to) {
+            log = log.filter(item => { return item.date.getTime() <= Date.parse(to) });
+        }
+
+        if (limit) {
+            log = log.filter((item, i) => i < req.query.limit);
+        }
+
         res.status(200).json({ 
             _id,
             username,
             count,
             log: log.map(exercise => { 
                 const { duration, description } = exercise;
+                // convert the date to an easier-to-read format
                 let date = new Date(exercise.date).toDateString();
                 return {
                     date,
