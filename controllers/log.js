a1061b4cb3f32b184ecbd8dd7f917110a9af1933
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-26 17:58:47 -0500

    Feat: Add count key value
    Count displays the log array length.

--- controllers/log.js
+++ controllers/log.js
@@ -6,9 +6,11 @@ const log = (req, res) => {
     User.findById(userId, (err, user) => {
         if (err) return console.error(`log error: ${err}`);
         const { _id, username, log } = user;
+        const count = log.length;
         res.status(200).json({ 
             _id,
             username,
+            count,
             log: log.map(exercise => { 
                 const { duration, description } = exercise;
                 let date = new Date(exercise.date).toDateString();
